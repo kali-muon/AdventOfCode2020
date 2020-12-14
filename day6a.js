1e4008822b4cdf08b3721eddb5ab08fd2cd91e31
Lukus <lukusaurelius@gmail.com>
2020-12-13 22:26:51 -0600

    Day 6 complete!

---
+++ day6a.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const fs = require('fs');
+const inputData = fs.readFileSync("./day6data").toString();
+
+// Recycled from my day 1 solution
+function removeCharacter(str, character) {
+  return(str.split("").map(x => x.replace(character, "")).join(""));
+}
+
+const groups = inputData.split('\n\n').map(x => removeCharacter(x, '\n')).map(y => y.split(''));
+
+//console.log(groups[0]);
+
+let numOfQuestions = []
+
+for(let i of groups) {
+  let questionsAsked = [];
+  for(let j of i) {
+    if(questionsAsked.indexOf(j) == -1) {
+      questionsAsked.push(j);
+    }
+    //console.log(j);
+    //console.log(questionsAsked);
+  }
+  numOfQuestions.push(questionsAsked.length);
+}
+
+console.log(`Number of questions list: ${numOfQuestions}`);
+console.log(`Total questions answered yes: ${numOfQuestions.reduce((a,b) => a + b)}`)
---
+++ day6b.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const fs = require('fs');
+const inputData = fs.readFileSync("./day6data").toString();
+
+// Recycled from my day 1 solution
+function removeCharacter(str, character) {
+  return(str.split("").map(x => x.replace(character, "")).join(""));
+}
+
+function getMatching(arr1, arr2) {
+  let matchingValues = [];
+  for(let i of arr1) {
+    //console.log(`checking for ${i} in ${arr2}`)
+    if(arr2.indexOf(i) != -1) {
+      matchingValues.push(i);
+    }
+  }
+  return(matchingValues);
+}
+
+const groups = inputData.split('\n\n').map(x => x.split('\n').map(y => y.split('')));
+
+let numOfQuestions = []
+
+// probably could have done this with another reduce tbh
+
+for(let i of groups) {
+  numOfQuestions.push(i.reduce((a,b) => getMatching(a,b)).length);
+}
+
+// ok I tried (it didn't work)
+//numOfQuestions = groups.reduce((a,b) => a.concat(b[0].reduce((c,d) => getMatching(c,d).length)));
+
+console.log(`Number of questions list: ${numOfQuestions}`);
+console.log(`Total questions answered yes: ${numOfQuestions.reduce((a,b) => a + b)}`)
